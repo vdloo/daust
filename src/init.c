c6629cee05cf3a236cc2b9b4a979191a6551ad02
Rick van de Loo <rickvandeloo@gmail.com>
2014-07-11 12:44:30 +0000

    preparing for check_command

--- src/init.c
+++ src/init.c
@@ -57,9 +57,30 @@ void broadcast_command(char *command)
 	free(buf);
 }
 
-int run_command(char *command)
+int verify_node(char *local, char *foreign)
 {
-	
+	// replace this with some kind of public/private 
+	// key mechanism in the // future
+	return strstr(local, foreign) ? 1 : 0;
+}
+
+
+int check_command(struct nodeinfo *nfo)
+{
+	int i = 0;
+	if (strstr(nfo->command, "all")) {
+		// do one of the 'all' commands
+	} else if (strstr(nfo->command, "remote")) {
+		// do one of the 'remote' commands
+	} else if (strstr(nfo->command, "group")) {
+		// do one of the 'group' commands
+	} else {
+		// do one of the 'self' commands
+		if (verify_node(nfo->identifier, config->identifier)) {
+//			i = run_command(nfo->command);
+		}
+	}
+	return i;
 }
 
 // the function to process incoming data
@@ -79,6 +100,9 @@ char *incoming_callback(char *buf)
 	nfo = nl->info;
 	if (nfo->command && !strstr(nfo->command, na)) {
 		//try to run command
+		if (!check_command(nfo) && config->verbosity) {
+			printf("Declined incoming command\n");
+		}
 	}
 }
 
